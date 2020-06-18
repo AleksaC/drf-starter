9fac6b380bda9a6e61484a64e98165689052ab6d
AleksaC <aleksacukovic1@gmail.com>
2020-06-18 14:58:29 +0200

    Update runtests.sh and use it in CI

--- project/runtests.sh
+++ project/runtests.sh
@@ -1,4 +1,10 @@
 #!/usr/bin/env bash
 set -eou pipefail
 
-coverage run manage.py test --settings=server.settings.test --keepdb && coverage report
+coverage run manage.py test --settings=server.settings.test --keepdb
+
+if [[ -v COVERALLS_REPO_TOKEN ]]; then
+    coveralls
+fi
+
+coverage report
